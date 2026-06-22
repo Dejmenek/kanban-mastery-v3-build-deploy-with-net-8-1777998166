6a0aa86b02a671149e7c530960a011360e767406
Dejmenek <dejmen14567@gmail.com>
2026-06-22 16:02:48 +0200

    feat: add migration for board name and description

---
+++ src/Kanban.API/Migrations/20260622135834_AddNameAndDescriptionToBoard.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kanban.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNameAndDescriptionToBoard : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Boards",
+                type: "TEXT",
+                maxLength: 300,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Boards",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Boards");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Boards");
+        }
+    }
+}
