c9cfb455fa22f527f846b49a7c4906537a4680dc
Dejmenek <dejmen14567@gmail.com>
2026-06-27 18:50:44 +0200

    feat: add board endpoint retrieving all boards for authenticated user

--- src/Kanban.API/Program.cs
+++ src/Kanban.API/Program.cs
@@ -1,4 +1,5 @@
 using Kanban.API.Data;
+using Kanban.API.Endpoints;
 using Kanban.API.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -30,24 +31,6 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.MapBoardEndpoints();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
