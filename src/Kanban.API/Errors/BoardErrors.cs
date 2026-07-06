dba6a8d75e2865254b025f7e3584e569d5320dba
Dejmenek <dejmen14567@gmail.com>
2026-07-06 15:26:21 +0200

    feat: implement board endpoint for adding new members

---
+++ src/Kanban.API/Errors/BoardErrors.cs
@@ -0,0 +1,18 @@
+using Kanban.API.Common;
+
+namespace Kanban.API.Errors;
+
+public static class BoardErrors
+{
+    public static Error NotFound(int boardId) =>
+        Error.NotFound("Board.NotFound", $"Board with id '{boardId}' was not found.");
+
+    public static Error MissingMemberIdentifier =>
+        Error.Validation("Board.MissingMemberIdentifier", "Either UserId or Email must be provided.");
+
+    public static Error UserNotFound(string identifier) =>
+        Error.NotFound("Board.UserNotFound", $"User '{identifier}' was not found.");
+
+    public static Error AlreadyMember =>
+        Error.Conflict("Board.AlreadyMember", "User is already a member of the board.");
+}
