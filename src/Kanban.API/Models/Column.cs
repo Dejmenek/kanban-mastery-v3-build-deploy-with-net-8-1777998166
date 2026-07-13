5a55ea95ec08878f3ad28dfb01f49692fc343089
Dejmenek <dejmen14567@gmail.com>
2026-07-13 19:02:13 +0200

    feat: add card and column details

--- src/Kanban.API/Models/Column.cs
+++ src/Kanban.API/Models/Column.cs
@@ -4,6 +4,9 @@ public class Column
 {
     public int Id { get; set; }
     public int BoardId { get; set; }
+    public string Title { get; set; } = null!;
+    public string? Description { get; set; }
+    public int Position { get; set; }
 
     public ICollection<Card> Cards { get; set; } = [];
     public Board Board { get; set; } = null!;
