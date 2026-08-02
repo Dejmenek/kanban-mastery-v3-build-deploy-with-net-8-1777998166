32fac4175d57d5d786f80f6e170297e05d5dfc69
Dejmenek <dejmen14567@gmail.com>
2026-08-02 20:50:44 +0200

    feat(client): add modular routing structure with lazy loading

--- client/kanban-web/src/app/app.routes.ts
+++ client/kanban-web/src/app/app.routes.ts
@@ -3,6 +3,21 @@ import { authGuard } from './core/auth/guards/auth-guard';
 
 export const routes: Routes = [
   {
+    path: '',
+    loadComponent: () => import('./layout/shell/shell').then((m) => m.Shell),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./features/home/home').then((m) => m.Home),
+      },
+      {
+        path: 'dashboard',
+        canActivate: [authGuard],
+        loadComponent: () => import('./features/dashboard/dashboard').then((m) => m.Dashboard),
+      },
+    ],
+  },
+  {
     path: 'login',
     loadComponent: () => import('./features/auth/pages/login/login').then((m) => m.Login),
   },
