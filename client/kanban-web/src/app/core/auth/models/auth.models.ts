03d0f846e1f932b16b3db17e0165b7a91d4998a4
Dejmenek <dejmen14567@gmail.com>
2026-08-02 12:57:50 +0200

    refactor: move auth logic to separate AuthService

--- client/kanban-web/src/app/core/auth/models/auth.models.ts
+++ client/kanban-web/src/app/core/auth/models/auth.models.ts
@@ -3,14 +3,14 @@ export interface LoginRequest {
   password: string;
 }
 
+export interface RegisterRequest {
+  email: string;
+  password: string;
+}
+
 export interface LoginResponse {
   tokenType: string;
   accessToken: string;
   expiresIn: number;
   refreshToken: string;
-}
-
-export interface RegisterRequest {
-  email: string;
-  password: string;
 }
