c3b631551ffb206a2d1b267fc60ef5b37d6aeb55
Dejmenek <dejmen14567@gmail.com>
2026-08-05 12:37:01 +0200

    feat(client): add drag-and-drop support to board-card component

--- client/kanban-web/src/app/features/boards/components/board-card/board-card.ts
+++ client/kanban-web/src/app/features/boards/components/board-card/board-card.ts
@@ -1,12 +1,13 @@
 import { Component, input } from '@angular/core';
 import { Avatar } from '../../../../shared/components/avatar/avatar';
 import { CardResponse } from '../../models/board.models';
+import { CdkDrag } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-board-card',
   templateUrl: './board-card.html',
   styleUrl: './board-card.css',
-  imports: [Avatar],
+  imports: [Avatar, CdkDrag],
 })
 export class BoardCard {
   card = input.required<CardResponse>();
