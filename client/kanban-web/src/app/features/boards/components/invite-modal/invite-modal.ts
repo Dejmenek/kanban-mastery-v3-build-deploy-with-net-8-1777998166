928c9b3fe24dc5d39fef60e3a7899cc009084351
Dejmenek <dejmen14567@gmail.com>
2026-08-05 16:31:54 +0200

    feat(client): add invite modal for board member management

---
+++ client/kanban-web/src/app/features/boards/components/invite-modal/invite-modal.ts
@@ -0,0 +1,55 @@
+import { Component, inject, signal } from '@angular/core';
+import { DialogRef, DIALOG_DATA } from '@angular/cdk/dialog';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
+import { finalize } from 'rxjs';
+import { BoardService } from '../../services/board.service';
+import { AddBoardMemberRequest, BoardMemberResponse } from '../../models/board.models';
+
+export interface InviteModalData {
+  boardId: number;
+}
+
+@Component({
+  selector: 'app-invite-modal',
+  templateUrl: './invite-modal.html',
+  styleUrl: './invite-modal.css',
+  imports: [ReactiveFormsModule],
+})
+export class InviteModal {
+  private data = inject<InviteModalData>(DIALOG_DATA);
+  protected dialogRef = inject<DialogRef<BoardMemberResponse>>(DialogRef);
+  private boardService = inject(BoardService);
+  protected inviteForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
+  protected errorMessage = signal<string | null>(null);
+  protected isSubmitting = signal(false);
+
+  protected onSubmit() {
+    if (this.inviteForm.invalid) {
+      this.inviteForm.markAllAsTouched();
+      return;
+    }
+
+    this.errorMessage.set(null);
+    this.isSubmitting.set(true);
+
+    const addMemberRequest: AddBoardMemberRequest = {
+      email: this.inviteForm.value.email!,
+    };
+
+    this.boardService
+      .addMember(this.data.boardId, addMemberRequest)
+      .pipe(finalize(() => this.isSubmitting.set(false)))
+      .subscribe({
+        next: (member) => this.dialogRef.close(member),
+        error: (err: HttpErrorResponse) => this.errorMessage.set(this.extractErrorMessage(err)),
+      });
+  }
+
+  private extractErrorMessage(err: HttpErrorResponse): string {
+    if (typeof err.error === 'string' && err.error.length > 0) return err.error;
+    return 'Could not send invite. Please try again.';
+  }
+}
--- client/kanban-web/src/app/features/boards/services/board.service.ts
+++ client/kanban-web/src/app/features/boards/services/board.service.ts
@@ -1,7 +1,7 @@
 import { inject, Service } from '@angular/core';
 import { ApiService } from '../../../core/http/services/api.service';
 import { Observable } from 'rxjs';
-import { BoardDetailsResponse, BoardSummaryResponse, MoveCardRequest, MoveCardResponse } from '../models/board.models';
+import { AddBoardMemberRequest, BoardDetailsResponse, BoardMemberResponse, BoardSummaryResponse, MoveCardRequest, MoveCardResponse } from '../models/board.models';
 
 @Service()
 export class BoardService {
@@ -20,5 +20,10 @@ export class BoardService {
   moveCard(boardId: number, cardId: number, request: MoveCardRequest): Observable<MoveCardResponse> {
     return this.api
       .put<MoveCardRequest, MoveCardResponse>(`/api/boards/${boardId}/cards/${cardId}/position`, request);
+  }
+
+  addMember(boardId: number, request: AddBoardMemberRequest) {
+    return this.api
+      .post<AddBoardMemberRequest, BoardMemberResponse>(`/api/boards/${boardId}/members`, request);
   }
 }
