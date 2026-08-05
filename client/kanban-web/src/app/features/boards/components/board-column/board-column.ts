1224e0622113addcbc14370181d5ed5ed9f67150
Dejmenek <dejmen14567@gmail.com>
2026-08-05 12:38:29 +0200

    feat(client): add drag-and-drop support to board columns

--- client/kanban-web/src/app/features/boards/components/board-column/board-column.ts
+++ client/kanban-web/src/app/features/boards/components/board-column/board-column.ts
@@ -1,13 +1,15 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { BoardCard } from '../board-card/board-card';
-import { ColumnResponse } from '../../models/board.models';
+import { CardResponse, ColumnResponse } from '../../models/board.models';
+import { CdkDragDrop, CdkDropList } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-board-column',
   templateUrl: './board-column.html',
   styleUrl: './board-column.css',
-  imports: [BoardCard],
+  imports: [BoardCard, CdkDropList],
 })
 export class BoardColumn {
   column = input.required<ColumnResponse>();
+  dropped = output<CdkDragDrop<CardResponse[]>>();
 }
