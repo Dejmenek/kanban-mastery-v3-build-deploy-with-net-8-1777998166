ac2b1c27b422fcd983911a246a19c99cd5bf368b
Dejmenek <dejmen14567@gmail.com>
2026-08-14 14:26:09 +0200

    build(backend): add SQL Server support for production environments

--- src/Kanban.API/Program.cs
+++ src/Kanban.API/Program.cs
@@ -10,7 +10,17 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    if (builder.Environment.IsDevelopment())
+    {
+        options.UseSqlite(connectionString);
+    }
+    else
+    {
+        options.UseSqlServer(connectionString);
+    }
+});
 builder.Services.AddIdentityApiEndpoints<ApplicationUser>(options =>
 {
     options.User.RequireUniqueEmail = true;
